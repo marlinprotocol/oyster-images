2e730339737ebaa145477c63f5d2b2ac587e3cac
Prateek Reddy <prateekreddyy@gmail.com>
2023-08-09 16:11:18 +0400

    feat: target arch for enclave images can be specified

--- enclave-builder/entrypoint.sh
+++ enclave-builder/entrypoint.sh
@@ -4,9 +4,29 @@ dockerd &
 
 sleep 10
 
-/app/builder/enclave-builder --config /app/mount/config.json
+docker buildx create --name multiplatformEnclave --driver docker-container --bootstrap
+docker buildx use multiplatformEnclave
 
-docker image build -t enclave:latest .
+rm -rf /app/mount/enclave
+
+if [ -z "$TARGETARCH" ]
+then
+    RAW_ARCH=$(uname -m)
+    if [ "$RAW_ARCH" = "x86_64" ]
+    then
+        TARGETARCH="amd64"
+    elif [ "$RAW_ARCH" = "aarch64" ]
+    then
+        TARGETARCH="arm64"
+    else
+        echo "Unsupported architecture: $RAW_ARCH"
+        exit 1
+    fi
+fi
+
+/app/builder/enclave-builder --config /app/mount/config.json --arch $TARGETARCH
+docker buildx build --platform linux/$TARGETARCH -t enclave:latest --load .
+
 mkdir -p /app/mount/enclave
 mkdir -p /var/log/nitro_enclaves
 touch /var/log/nitro_enclaves/nitro_enclaves.log
